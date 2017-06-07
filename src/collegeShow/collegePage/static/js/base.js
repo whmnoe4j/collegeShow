f1f2bdfb77366cc8813856e400b3765236dc9ec2
zuoakang <944528536@qq.com>
2017-06-07 20:31:18 +0800

    表格样式修改

--- src/collegeShow/collegePage/static/js/base.js
+++ src/collegeShow/collegePage/static/js/base.js
@@ -1,7 +1,7 @@
-$(document).ready(function(e) {     
-
-    // alert($('.data-tbody tr').length)
-    // $('.data-tbody tr').each(function(){
-    //     alert(1)
-    // })
-});
+//$(document).ready(function(e) {     
+//
+//    // alert($('.data-tbody tr').length)
+//    // $('.data-tbody tr').each(function(){
+//    //     alert(1)
+//    // })
+//});
